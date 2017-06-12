c2e5602b8debce2b79f6d63cd99a922f4f4e486a
zbagz <zbegsazj@pwrby.com>
2017-06-12 18:43:26 -0400

    added test a

---
+++ src/test/java/io/proleap/vb6/asg/newtests/a/TestA.java
@@ -0,0 +1,79 @@
+package io.proleap.vb6.asg.newtests.a;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+
+import java.io.File;
+import java.util.Arrays;
+
+import org.junit.Test;
+
+import io.proleap.vb6.VbTestBase;
+import io.proleap.vb6.asg.metamodel.ClazzModule;
+import io.proleap.vb6.asg.metamodel.Module;
+import io.proleap.vb6.asg.metamodel.Program;
+import io.proleap.vb6.asg.metamodel.StandardModule;
+import io.proleap.vb6.asg.metamodel.TypeElement;
+import io.proleap.vb6.asg.metamodel.statement.enumeration.Enumeration;
+import io.proleap.vb6.asg.metamodel.statement.enumeration.EnumerationConstant;
+import io.proleap.vb6.asg.metamodel.statement.function.Function;
+import io.proleap.vb6.asg.metamodel.statement.property.get.PropertyGet;
+import io.proleap.vb6.asg.metamodel.type.VbBaseType;
+import io.proleap.vb6.asg.runner.impl.VbParserRunnerImpl;
+
+public class TestA extends VbTestBase {
+
+    @Test
+    public void test() throws Exception {
+        final File clsSoundInputFile = new File(
+                "src/test/resources/io/proleap/vb6/asg/newtests/a/clsSound.cls");
+
+        final File clsTestInputFile = new File(
+                "src/test/resources/io/proleap/vb6/asg/newtests/a/clsTest.cls");
+        
+        final File modGeneralInputFile = new File(
+                "src/test/resources/io/proleap/vb6/asg/newtests/a/modGeneral.bas");
+
+        final Program program = new VbParserRunnerImpl().analyzeFiles(Arrays.asList(clsTestInputFile,
+                modGeneralInputFile, clsSoundInputFile)); // note the exact order
+
+        {
+            final ClazzModule clsSound = program.getClazzModule("clsSound");
+            assertNotNull(clsSound);
+
+            {
+                final Function Sound_Play = clsSound.getFunction("Sound_Play");
+                assertNotNull(Sound_Play);
+                assertEquals(0, Sound_Play.getFunctionCalls().size());
+            }
+
+            {
+                final Function Sound_Stop = clsSound.getFunction("Sound_Stop");
+                assertNotNull(Sound_Stop);
+                assertEquals(0, Sound_Stop.getFunctionCalls().size());
+            }
+        }
+
+        {
+            final StandardModule modGeneral = program.getStandardModule("modGeneral");
+            assertNotNull(modGeneral);
+
+            {
+                final Enumeration eSoundAction = modGeneral.getEnumerations().get("eSoundAction");
+                assertNotNull(eSoundAction);
+
+                {
+                    final EnumerationConstant Sound_Play = eSoundAction.getEnumerationConstant("Sound_Play");
+                    assertNotNull(Sound_Play);
+                    assertEquals(1, Sound_Play.getEnumerationConstantCalls().size());
+                }
+
+                {
+                    final EnumerationConstant Sound_Stop = eSoundAction.getEnumerationConstant("Sound_Stop");
+                    assertNotNull(Sound_Stop);
+                    assertEquals(1, Sound_Stop.getEnumerationConstantCalls().size());
+                }
+            }
+        }
+    }
+}
